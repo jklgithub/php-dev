883a67134e12bebe2b1a821c805c40e416416557
蒋昆仑 <jiangkunlun@uboxol.com>
2015-10-21 16:38:54 +0800

    pi

--- app/Config/conf_dev/conf.php
+++ app/Config/conf_dev/conf.php
@@ -1,7 +1,7 @@
 <?php 
 $config['conf']['host'] 						= $_SERVER['HTTP_HOST'];
 
-$config['conf']['log']['path'] 					= '....';
+$config['conf']['log']['path'] 					= '/jkl/logs/php-dev/';
 $config['yees']['log']['logName'] 				= 'app';		//名称
 $config['yees']['log']['level'] 				= 6;				//记录级别
 $config['yees']['log']['service'] 				= 'dev';		//当前的服务点
